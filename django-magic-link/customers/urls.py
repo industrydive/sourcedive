c19d7bdfc8a40d197fabfb54f0fe65ba97073899
Greg Linch <greglinch@gmail.com>
2019-05-20 17:02:03 -0400

    remove django magic link

--- django-magic-link/customers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.customers_home_page, name="customers-home"),
-    url(r'^login/$',views.login_page, name="customers-login"),
-]
-
