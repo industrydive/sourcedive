922e8009776a77307794d30b11a7c1cc3cd92aeb
Greg Linch <greglinch@gmail.com>
2019-05-23 16:41:09 -0400

    add abstracted reference to STATIC_ROOT var

--- sourcedive/settings.py
+++ sourcedive/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = STATIC_ROOT_PATH
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Python social auth 
 AUTHENTICATION_BACKENDS = (
