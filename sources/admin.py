9fc0a65267a0bf68f22406f03ed824e7c3f6211a
Greg Linch <greglinch@gmail.com>
2019-08-09 17:00:22 -0400

    get interaction admin working with message in replacement field; still in progress for person admin -- throwing field not recognized error

--- sources/admin.py
+++ sources/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.db.models import Q
+from django.utils.html import format_html
 
 from .models import (
     Expertise,
@@ -27,19 +28,24 @@ class InteractionInline(admin.TabularInline):
 
 
 class InteractionAdmin(admin.ModelAdmin):
-    fields = ['privacy_level', 'date_time', 'interaction_type', 'interviewee', 'interviewer', 'notes', 'created_by']
+    fields = ['privacy_level', 'date_time', 'interaction_type', 'interviewee', 'interviewer']
     list_display = ['interviewee', 'interaction_type', 'privacy_level', 'date_time']
     filter_horizontal = ['interviewer']
-    readonly_fields = ['created_by']
 
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
-        # import pdb; pdb.set_trace()
+        """
+        If the privacy level is semi-private/searchable and the logged
+        in user is not the one who created the interaction, then remove
+        notes field for them
+        """
         obj = Interaction.objects.get(id=object_id)
-        # if the privacy level is semi-private/searchable and the logged
-        # in user is not the one who created the interaction
         if obj.privacy_level == 'searchable' and obj.created_by != request.user:
-            self.fields.pop(5)
+            self.fields = ['privacy_level', 'date_time', 'interaction_type', 'interviewee', 'interviewer', 'notes_semiprivate_display', 'created_by']
+            self.readonly_fields = ['created_by', 'notes_semiprivate_display']
+        else:
+            self.fields = ['privacy_level', 'date_time', 'interaction_type', 'interviewee', 'interviewer', 'notes', 'created_by']
+            self.readonly_fields = ['created_by']
         return self.changeform_view(request, object_id, form_url, extra_context)
 
 
@@ -55,6 +61,11 @@ class InteractionAdmin(admin.ModelAdmin):
             Q(created_by=request.user, privacy_level='private_individual')
         )
 
+    def notes_semiprivate_display(self, obj):
+        display_text = 'Please contact <strong>{}</strong> for this information'.format(obj.created_by)
+        return format_html(display_text)
+    notes_semiprivate_display.short_description = 'Notes'
+
 
 class OrganizationAdmin(admin.ModelAdmin):
     fields = ['name']
@@ -63,53 +74,6 @@ class OrganizationAdmin(admin.ModelAdmin):
 
 
 class PersonAdmin(admin.ModelAdmin):
-    fieldsets = (
-        ('Privacy', {
-            'fields': ('privacy_level',)
-        }),
-        ('General info', {
-            'fields': (
-                'prefix',
-                'pronouns',
-                'first_name',
-                'middle_name',
-                'last_name',
-                'title',
-                'industries',
-                'organization',
-                'website',
-                'type_of_expert',
-                'expertise',
-            ),
-        }),
-        ('Contact info', {
-            'fields': (
-                'email_address',
-                'phone_number_primary',
-                'phone_number_secondary',
-                'linkedin',
-                'twitter',
-                'skype',
-            ),
-        }),
-        ('Location info', {
-            'fields': (
-                'timezone',
-                'city',
-                'state',
-                'country',
-            ),
-        }),
-        ('Advanced info', {
-            'classes': ('collapse',),
-            'fields': (
-                'entry_method',
-                'entry_type',
-                'created_by',
-            ),
-        }),
-    )
-    # fields = ['privacy_level', 'prefix', 'pronouns', 'first_name', 'middle_name', 'last_name', 'type_of_expert', 'title', 'organization', 'website', 'expertise', 'email_address', 'phone_number_primary', 'phone_number_secondary', 'twitter', 'skype', 'language', 'timezone', 'city', 'state', 'country', 'notes', 'entry_method', 'entry_type', 'created_by']
     list_display = ['last_name', 'first_name', 'updated', 'created_by', 'privacy_level']
     list_filter = ['organization__name', 'expertise__name', 'timezone', 'city', 'state', 'privacy_level']
     search_fields = ['city', 'country', 'email_address', 'expertise__name', 'first_name', 'language', 'last_name', 'notes', 'organization', 'state', 'title', 'type_of_expert', 'twitter', 'website']
@@ -120,19 +84,127 @@ class PersonAdmin(admin.ModelAdmin):
     view_on_site = False  # THIS DOES NOT WORK CURRENTLY
     inlines = (InteractionInline,)
 
-    def timezone_abbrev(self, obj):
-        return obj.timezone
-    timezone_abbrev.short_description = ('Timezone offset')
+
+    def email_address_semiprivate_display(self, obj):
+        display_text = 'Please contact <strong>{}</strong> for this information'.format(obj.created_by)
+        return format_html(display_text)
+    email_address_semiprivate_display.short_description = 'Email address'
 
 
-    # def change_view(self, request, object_id, form_url='', extra_context=None):
-    #     # import pdb; pdb.set_trace()
-    #     obj = Person.objects.get(id=object_id)
-    #     # if the privacy level is semi-private/searchable and the logged
-    #     # in user is not the one who created the interaction
-    #     if obj.privacy_level == 'searchable' and obj.created_by != request.user:
-    #         self.fields.pop(5)  # UPDATE to remove contact info
-    #     return self.changeform_view(request, object_id, form_url, extra_context)
+    def phone_number_primary_semiprivate_display(self, obj):
+        display_text = 'Please contact <strong>{}</strong> for this information'.format(obj.created_by)
+        return format_html(display_text)
+    phone_number_primary_semiprivate_display.short_description = 'Phone number primary'
+
+
+    def phone_number_secondary_semiprivate_display(self, obj):
+        display_text = 'Please contact <strong>{}</strong> for this information'.format(obj.created_by)
+        return format_html(display_text)
+    phone_number_secondary_semiprivate_display.short_description = 'Phone number secondary'
+
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        """
+        If the privacy level is semi-private/searchable and the logged
+        in user is not the one who created the interaction, then remove
+        notes field for them
+        """
+        obj = Person.objects.get(id=object_id)
+        if obj.privacy_level == 'searchable' and obj.created_by != request.user:
+            self.fieldsets = (
+                ('Privacy', {
+                    'fields': ('privacy_level',)
+                }),
+                ('General info', {
+                    'fields': (
+                        'prefix',
+                        'pronouns',
+                        'first_name',
+                        'middle_name',
+                        'last_name',
+                        'title',
+                        'industries',
+                        'organization',
+                        'website',
+                        'type_of_expert',
+                        'expertise',
+                    ),
+                }),
+                ('Contact info', {
+                    'fields': (
+                        # 'email_address_semiprivate_display',
+                        # 'phone_number_primary_semiprivate_display',
+                        # 'phone_number_secondary_semiprivate_display',
+                        'linkedin',
+                        'twitter',
+                        'skype',
+                    ),
+                }),
+                ('Location info', {
+                    'fields': (
+                        'timezone',
+                        'city',
+                        'state',
+                        'country',
+                    ),
+                }),
+                ('Advanced info', {
+                    'classes': ('collapse',),
+                    'fields': (
+                        'entry_method',
+                        'entry_type',
+                        'created_by',
+                    ),
+                }),
+            )
+        else:
+            self.fieldsets = (
+                ('Privacy', {
+                    'fields': ('privacy_level',)
+                }),
+                ('General info', {
+                    'fields': (
+                        'prefix',
+                        'pronouns',
+                        'first_name',
+                        'middle_name',
+                        'last_name',
+                        'title',
+                        'industries',
+                        'organization',
+                        'website',
+                        'type_of_expert',
+                        'expertise',
+                    ),
+                }),
+                ('Contact info', {
+                    'fields': (
+                        'email_address',
+                        'phone_number_primary',
+                        'phone_number_secondary',
+                        'linkedin',
+                        'twitter',
+                        'skype',
+                    ),
+                }),
+                ('Location info', {
+                    'fields': (
+                        'timezone',
+                        'city',
+                        'state',
+                        'country',
+                    ),
+                }),
+                ('Advanced info', {
+                    'classes': ('collapse',),
+                    'fields': (
+                        'entry_method',
+                        'entry_type',
+                        'created_by',
+                    ),
+                }),
+            )
+        return self.changeform_view(request, object_id, form_url, extra_context)
 
 
     def get_queryset(self, request):
