9cc2b013e0c0c9b07e4c872f349fac0c262f1823
Greg Linch <greglinch@gmail.com>
2019-12-16 16:45:53 -0500

    update m2m name fields to be required

--- sources/models.py
+++ sources/models.py
@@ -22,7 +22,7 @@ class BasicInfo(models.Model):
 
 
 class Expertise(BasicInfo):
-    name = models.CharField(max_length=255, null=True, blank=True, unique=True, verbose_name='Type of expertise')
+    name = models.CharField(max_length=255, null=True, blank=False, unique=True, verbose_name='Type of expertise')
 
     class Meta:
         verbose_name_plural = 'Expertise'
@@ -32,7 +32,7 @@ class Expertise(BasicInfo):
 
 
 class Industry(BasicInfo):
-    name = models.CharField(max_length=255, null=True, blank=True, unique=True, verbose_name='Industry name')
+    name = models.CharField(max_length=255, null=True, blank=False, unique=True, verbose_name='Industry name')
 
     class Meta:
         verbose_name_plural = 'Industries'
@@ -42,7 +42,7 @@ class Industry(BasicInfo):
 
 
 class Organization(BasicInfo):
-    name = models.CharField(max_length=255, null=True, blank=True, unique=True, verbose_name='Organization name')
+    name = models.CharField(max_length=255, null=True, blank=False, unique=True, verbose_name='Organization name')
     # location = models.ForeignKey(Location, null=True, blank=True)
     # website = models.URLField(max_length=200, null=True, blank=True)
 
