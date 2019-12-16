777ee1c47ef3de973c0486a0a72b1a348cfbe81c
Greg Linch <greglinch@gmail.com>
2019-12-16 16:17:20 -0500

    remove pipeline to add perms to any whitelisted users

--- sourcedive/settings.py
+++ sourcedive/settings.py
@@ -141,7 +141,6 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
-    'sources.pipeline.add_permissions_to_new_whitelisted_users',
 )
 
 LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale/')]
