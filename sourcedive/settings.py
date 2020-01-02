b4256a2d8018a5dc58592501cd50cad621b2ef3e
Greg Linch <greglinch@gmail.com>
2020-01-02 16:36:29 -0500

    uncomment social django middleware

--- sourcedive/settings.py
+++ sourcedive/settings.py
@@ -41,7 +41,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # 'debug_toolbar.middleware.DebugToolbarMiddleware',
-    # 'social_django.middleware.SocialAuthExceptionMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'sourcedive.urls'
