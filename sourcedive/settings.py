43b8666dca2790178fa65c057cc862823d16068a
Greg Linch <greglinch@gmail.com>
2020-01-06 14:15:11 -0500

    fix settings for handling media + static

--- sourcedive/settings.py
+++ sourcedive/settings.py
@@ -120,7 +120,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'media'),
+    os.path.join(BASE_DIR, 'media'),
 )
 
 # Python social auth 
