39f81be83180cd87cf4a610481ee552679c91ab8
Miriam Sexton <miriam@industrydive.com>
2020-01-13 10:35:01 -0500

    Update sources/admin.py
    Co-Authored-By: Greg Linch <greglinch@users.noreply.github.com>

--- sources/admin.py
+++ sources/admin.py
@@ -224,7 +224,7 @@ class OrganizationFilter(SimpleListFilter):
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = ['name', 'updated', 'created_by', 'privacy_level']
-    list_filter = [IndustryFilter, ExpertiseFilter, OrganizationFilter, 'timezone', 'city', 'state', 'privacy_level']
+    list_filter = [IndustryFilter, ExpertiseFilter, OrganizationFilter, 'city', 'state', 'privacy_level']
     search_fields = ['city', 'country', 'email_address', 'expertise__name', 'first_name', 'language', 'name', 'notes', 'organization', 'state', 'title', 'type_of_expert', 'twitter', 'website']
     filter_horizontal = ['expertise', 'industries', 'organization']
     readonly_fields = ['entry_method', 'entry_type', 'created_by', 'updated']
